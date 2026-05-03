393ca7fe124599ba12affab808613ea53d7f8871
Michael Dear <mjdear68@gmail.com>
2026-05-03 14:57:14 +1000

    Added img_group() function to copy images to directories by date/hour.

--- imgfuncs.sh
+++ imgfuncs.sh
@@ -16,3 +16,20 @@ img_rename() {
              -d "%Y%m%d_%H%M%S%%-c.%%le" "$input"
 }
 
+
+img_group() {
+	local input=$1    # input folder
+	local output=$2   # output folder
+
+	# Validate input parameters
+	if [ $# -ne 2 ]; then
+	echo "Error: Function requires exactly 2 arguments (input folder and output folder)"
+	return 1
+	fi
+	
+	# Run the command
+    exiftool -@ ext.args "-Directory<CreateDate" \
+             -r -o . \
+			 -d "$output%Y-%m-%d-_%H00" "$input"
+             
+}
