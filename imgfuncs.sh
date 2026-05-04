154e086f1fd3486cd3789ea478c9ec2d69f8737d
Michael Dear <mjdear68@gmail.com>
2026-05-04 20:25:16 +1000

    Removed -description from img_write_desc. Added /oppo/ to .gitignore (to be deleted later)

--- imgfuncs.sh
+++ imgfuncs.sh
@@ -30,23 +30,23 @@ img_group() {
 	# Run the command
     exiftool -@ ext.args "-Directory<CreateDate" \
              -r -o . \
-			 -d "$output%Y-%m-%d_%H00" "$input"
+			 -d "$output/%Y-%m-%d_%H00" "$input"
              
 }
 
 img_write_desc() {
 	local title=$1    
-	local description=$2   
-	local keywords=$3
-	local input=$4 # input folder
+	local keywords=$2 	
+	local input=$3 		# input folder
 
 	# Validate input parameters
-	if [ $# -ne 4 ]; then
-	echo "Error: Function requires exactly 4 arguments (title, description, keywords, and input folder)"
+	if [ $# -ne 3 ]; then
+	echo 'Error: Function requires exactly 3 arguments (title, keywords, and input folder)\n
+	Example: "Autumn leaves in a suburban street" "landscape; trees; autumn" ./input'
 	return 1
 	fi
 	
 	# Run the command
-    exiftool -@ ext.args -overwrite_original -Title="$title" -Description="$description" -Keywords="$keywords" "$input"
+    exiftool -@ ext.args -overwrite_original -Title="$title" -Keywords="$keywords" "$input"
              
 }
