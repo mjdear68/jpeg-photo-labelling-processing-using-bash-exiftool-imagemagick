580d387ee6eb94fef617d5281b2a06651c2bd2a9
mjdear <mjdear68@gmail.com>
2026-06-01 22:11:20 +1000

    Added -CreateDate and -DateTimeOriginal to vid_conv. Still have time zone issues.

--- vidfuncs.sh
+++ vidfuncs.sh
@@ -5,7 +5,7 @@
 vid_conv() {
     # Safety Check: Ensure all arguments are provided
     if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-        echo "Usage: vid_conv [input_video] [reference_image] [compression_level] "
+        echo "Usage: vid_conv [input_video] [reference_image] [compression_level]"
         echo "Example: vid_conv ./input.mp4 ./reference.jpg 23"
         return 1
     fi
@@ -23,6 +23,6 @@ vid_conv() {
     # Convert the video using ffmpeg with the specified compression level and write to the same directory as the original video with the same name but .mp4 extension
      ffmpeg -i "$vid_in" -c:v libx265 -crf "$comp_level" -preset slow "$vid_converted"
 
-    # Copy the GPS metadata from the reference image to the converted video using exiftool
-     exiftool -overwrite_original -tagsfromfile "$ref_img" -CreateDate -GPSLatitude* -GPSLongitude* -GPSAltitude* "$vid_converted"
+    # Copy the GPS and creation date with time zone metadata from the reference image to the converted video using exiftool
+     exiftool -overwrite_original -tagsfromfile "$ref_img" -CreateDate -DateTimeOriginal -GPSLatitude* -GPSLongitude* -GPSAltitude* "$vid_converted"
 }
